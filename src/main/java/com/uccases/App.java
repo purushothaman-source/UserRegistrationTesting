c8c25724a39b09e4311e1489406c3e05fc959476
purushothaman-source <purushoth99jun@gmail.com>
2021-01-18 09:28:54 +0530

    ADDED : Welcome message

---
+++ src/main/java/com/uccases/App.java
@@ -0,0 +1,13 @@
+package com.uccases;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to UserRegistration!" );
+    }
+}
